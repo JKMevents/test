12592aad0c373b9ea0720c27f25e93fdf1f28d7c
Shubham <jkmeventsofficial@gmail.com>
2024-02-13 17:27:43 +0530

    testing first page

--- JKMapp/urls.py
+++ JKMapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.mambers, name="Home"),
+        path("", views.mambers, name="index_page"),
         path("Home/", views.Home, name="home")
 ]
 
--- JKMapp/views.py
+++ JKMapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def mambers(request):
+    
     return HttpResponse("Hello world!")
 
 
 def Home(request):
-    return HttpResponse("testing")
+    return render(
+        request,
+        "home.html",
+        {}
+    )
+
