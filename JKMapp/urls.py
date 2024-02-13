bbc7db6b763e7437515e45a8c02afc3399aad323
Shubham <jkmeventsofficial@gmail.com>
2024-02-13 23:21:46 +0530

    static testing 9

--- JKMapp/urls.py
+++ JKMapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
         path("", views.Home, name="home")
-]
+] + staticfiles_urlpatterns()
