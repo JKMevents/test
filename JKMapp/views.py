507e945ca01073f38587542669867769f537e9c4
Shubham <jkmeventsofficial@gmail.com>
2024-02-13 22:12:54 +0530

    static testing 4

--- JKMapp/views.py
+++ JKMapp/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 
-def mambers(request):
-    
-    return HttpResponse("Hello world!")
 
 
 def Home(request):
+   
     return render(
         request,
         "home.html",
