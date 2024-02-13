31c9a47c8e2b48de69804cee34b9973e16e00ee7
Shubham <jkmeventsofficial@gmail.com>
2024-02-13 17:57:11 +0530

    taking everything to front page

--- JKMapp/urls.py
+++ JKMapp/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.mambers, name="index_page"),
-        path("Home/", views.Home, name="home")
+        path("", views.Home, name="home")
 ]
-
